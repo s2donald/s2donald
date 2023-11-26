d7929117caaea7526e5129a74b408c3ddeacd87a
s2donald <s2donald@uwaterloo.ca>
2023-11-25 21:26:30 -0500

    Added my full website

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Stephen</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I like architecting cloud based solutions, building<br className='sm:block hidden' />ML models and AI based applications! <br className='sm:block hidden' />
+            I like architecting cloud based solutions, building<br className='sm:block hidden' /> ML models and AI applications! <br className='sm:block hidden' />
           </p>
         </div>
       </div>
